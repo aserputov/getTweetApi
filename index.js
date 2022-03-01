ed05d7678f168cdf431e3adacfd4826570c2c261
Anatoliy Serputoff <65831678+aserputov@users.noreply.github.com>
2022-03-01 17:46:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var config = {
   url: "https://api.twitter.com/2/tweets/1456335161377927175?tweet.fields=created_at,attachments&expansions=author_id",
   headers: {
     Authorization: "Bearer Code HERE ",
-    Cookie: "guest_id=v1%3A164401173817947613",
+    Cookie: "Guest-id",
   },
 };
 
